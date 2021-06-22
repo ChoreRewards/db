dd0fd7416bdacba1169b7068d3156c02db75f9bc
Simon Drake <simondrake1990@gmail.com>
2021-06-22 21:03:54 +0100

    Make unique fields unique

--- migrations/000001_create_tasks_table.up.sql
+++ migrations/000001_create_tasks_table.up.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS "tasks" (
   "id" SERIAL PRIMARY KEY NOT NULL,
   "category_id" INT,
   "assignee_id" INT,
-  "name" VARCHAR(255) NOT NULL,
+  "name" VARCHAR(255) NOT NULL UNIQUE,
   "description" TEXT NOT NULL DEFAULT '',
   "points" INT DEFAULT 0,
   "is_repeatable" BOOLEAN DEFAULT FALSE
--- migrations/000002_create_catories_table.up.sql
+++ migrations/000002_create_catories_table.up.sql
@@ -2,6 +2,6 @@ CREATE TABLE IF NOT EXISTS "categories" (
   "id" SERIAL PRIMARY KEY NOT NULL,
   "owner_id" INT DEFAULT NULL,
   "color" VARCHAR(255) NOT NULL DEFAULT '',
-  "name" VARCHAR(255) NOT NULL,
+  "name" VARCHAR(255) NOT NULL UNIQUE,
   "description" TEXT NOT NULL DEFAULT ''
 );
